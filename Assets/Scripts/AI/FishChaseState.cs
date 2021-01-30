8667e35267a9803bc28e371e03cfc81639aeaa9e
Justin Liles <Justin.Liles@outlook.com>
2021-01-30 17:12:07 -0600

    Updated Fish AI Chase State

--- Assets/Scripts/AI/FishChaseState.cs
+++ Assets/Scripts/AI/FishChaseState.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 [System.Serializable]
 public class FishChaseState : State
 {
+    Timer ignoreTime;
+
     public override void OnStart()
     {
+        StateMachine.transform.Translate(GameObject.FindGameObjectWithTag("Player").transform.position * Time.deltaTime);
+        ignoreTime.TimeOut.AddListener(StopChaseing);
+        ignoreTime.StartTimer(3.0f);
+
         base.OnStart();
+        Debug.Log("Starting Chase State");
     }
 
     public override void UpdateState(float dt)
@@ -17,6 +24,16 @@ public class FishChaseState : State
 
     public override void OnExit()
     {
+        ignoreTime.TimeOut.RemoveListener(StopChaseing);
         base.OnExit();
+
+        Debug.Log("Exitting Chase State");
+    }
+
+    void StopChaseing()
+    {
+        StateMachine.transform.Rotate(0, 0, 180f);
+        StateMachine.ChangeState(nameof(FishWanderState));
+        // Change to Ignore State
     }
 }
