cfe62780a8a2259abe748ad32275434b48a20ae2
Blake-Henderson <bmhenderson101@gmail.com>
2021-01-30 13:49:08 -0600

    Added keeping the player character to stay on screen

---
+++ Assets/Scripts/Player_Controls/Boundries.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//slightly edited code from:https://pressstart.vip/tutorials/2018/06/28/41/keep-object-in-bounds.html
+public class Boundries : MonoBehaviour
+{
+    public Camera MainCamera;
+    public GameObject body;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = body.transform.GetComponent<MeshRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = body.transform.GetComponent<MeshRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
