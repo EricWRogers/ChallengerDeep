aa30bf884e29ca21a9cac937aab007acd096f8cf
EricWRogers <yerick147083@gmail.com>
2021-01-30 09:54:45 -0600

    working on fish ai

---
+++ Assets/Scripts/Helper/FSM/FiniteStateMachine.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+public abstract class FiniteStateMachine : MonoBehaviour
+{
+    protected IState state = null;
+
+    public void SetState(IState _state)
+    {
+        if(_state == null)
+            return;
+        if (state != null)
+            state.Exit();
+        
+        state = _state;
+
+        state.Start();
+    }
+
+    public void FixedUpdate(float _dt)
+    {
+        if(state == null)
+            return;
+        
+        state.UpdateState(_dt);
+    }
+}
