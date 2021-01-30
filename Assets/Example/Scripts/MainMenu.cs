8a05fcf8acdae9535bb8c348d17721789ca2d66c
Jonathan B <cloudwolf09@gmail.com>
2021-01-30 11:59:32 -0600

    Added Main Menu - Not Functional

---
+++ Assets/Example/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string firstlevel;
+    public GameObject optionsScreen;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstlevel);
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+    }
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
