b5ec3c6a47090c1bd6bdc9d1f44a484f42f932fa
Jonathan B <cloudwolf09@gmail.com>
2021-01-30 17:03:11 -0600

    Main Menu - Graphics Adjusters (Audio In Progress)

---
+++ Assets/Scripts/UI/Menus/OptionsMenu.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Toggle fullscreenTog, vsyncTog;
+
+    public ResItem[] resolutions;
+
+    public int selectedResolution;
+
+    public Text resolutionLabel;
+
+    public AudioMixer theMixer;
+
+    public Slider masterSlider, musicSlider, sfxSlider;
+    public Text masterLabel, musicLabel, sfxLabel;
+
+    void Start()
+    {
+        fullscreenTog.isOn = Screen.fullScreen;
+
+        if(QualitySettings.vSyncCount == 0)
+        {
+            vsyncTog.isOn = false;
+        }
+        else
+        {
+            vsyncTog.isOn = true;
+        }
+
+        //search for resolution in list
+        bool foundRes = false;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            if(Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundRes = true;
+                selectedResolution = i;
+                UpdateResLabel();
+            }
+        }
+
+        if (!foundRes)
+        {
+            resolutionLabel.text = Screen.width.ToString() + " x " +  Screen.height.ToString();
+        }
+    }
+
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if(selectedResolution > resolutions.Length - 1)
+        {
+            selectedResolution = resolutions.Length - 1;
+        }
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
+    public void ApplyGraphics()
+    {
+        //apply fullscreen
+        //Screen.fullScreen = fullscreenTog.isOn;
+
+        if (vsyncTog.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        //set resolution
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenTog.isOn);
+    }
+
+    public void SetMasterVolume()
+    {
+        masterLabel.text = (masterSlider.value + 80).ToString();
+    }
+
+    public void SetMusicVolume()
+    {
+
+    }
+
+    public void SetSFxVolume()
+    {
+
+    }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
+}
