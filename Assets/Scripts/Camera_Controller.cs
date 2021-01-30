e42ba69ba4b73f88447380608ed3d2da868e7bf2
Blake-Henderson <bmhenderson101@gmail.com>
2021-01-29 20:45:24 -0600

    Base sorting of folders and player+camera scripts
    I moved all the example stuff into a folder named "Example" and made the start of the player controller as well as the camera controller.

---
+++ Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    /// <summary>
+    /// This is the player game object that the camera is binded to
+    /// </summary>
+    public GameObject player;
+
+    /// <summary>
+    /// Stores the offset value for the camera
+    /// </summary>
+    private Vector3 offset;
+
+    /// <summary>
+    /// At start up the offset is determined by the player object's position
+    /// </summary>
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    /// <summary>
+    /// The speed of the player character
+    /// </summary>
+    public float speed;
+    /// <summary>
+    /// The current size of the player
+    /// </summary>
+    public float size;
+    /// <summary>
+    /// This determines if the player can move.
+    /// </summary>
+    public bool stop = true;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        //stop = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 mouseInWorld = Camera.main.ScreenToWorldPoint(new Vector3
+            (Input.mousePosition.x, Input.mousePosition.y, 10));
+        rb.transform.LookAt(mouseInWorld);
+        var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPos.z = transform.position.z;
+        if (stop != true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+    }
+}
