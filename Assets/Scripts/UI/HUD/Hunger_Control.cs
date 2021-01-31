ee183dd0faecae30161ffc9b3d91d7733383e7b2
Jonathan B <cloudwolf09@gmail.com>
2021-01-31 01:36:08 -0600

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/UI/HUD/Hunger_Control.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Hunger_Control : MonoBehaviour
+{
+
+    public Slider hungerSlider;
+    public float hunger;
+    public float hungerOverTime;
+    public GameObject playa;
+
+    public void Update()
+    {
+        hunger -= hungerOverTime * Time.deltaTime;
+
+        if(hunger <= 0)
+        {
+            //GAME OVER
+            Debug.Log("You Died");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    public void SetMaxHunger()
+    {
+        hungerSlider.maxValue = hunger;
+        hungerSlider.value = hunger;
+    }
+
+    public void SetHunger()
+    {
+        hungerSlider.value = hunger;
+    }
+}
---
+++ Assets/Scripts/UI/HUD/Progress_Control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progress_Control : MonoBehaviour
+{
+    public Slider progressSlider;
+
+    public void SetMinProgress(int progress)
+    {
+        progressSlider.minValue = progress;
+        progressSlider.value = progress;
+    }
+
+    public void SetHunger(int progress)
+    {
+        progressSlider.value = progress;
+    }
+}
