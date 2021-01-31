516acea6e8d29fbc5475f1bb05f3024e29d86d13
Justin Liles <justin.liles@live.com>
2021-01-31 00:26:53 -0600

    Added Decision state to AI

---
+++ Assets/Scripts/AI/FishDecisionState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishDecisionState : State
+{
+    private float randomDecision;
+    public override void OnStart()
+    {
+        base.OnStart();
+
+        // 0 = Attack and 1 = Flee
+        randomDecision = Random.Range(0,1);
+        if(Player_Controller.Instance.GetComponent<Size_Control>().sizeLevel > StateMachine.gameObject.GetComponent<FishAIStateMachine>().Level)
+        {
+            //StateMachine.ChangeState(nameof(FishFleeState));
+        }
+    }
+}
